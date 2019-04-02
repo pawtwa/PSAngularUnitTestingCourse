b8f66d958c7a6fb41816f205da7cf8e6c1597ce5
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-04-02 11:34:09 +0200

    Isolated Unit Test - Testing a Pipe

---
+++ src/app/strength/strength.pipe.spec.ts
@@ -0,0 +1,27 @@
+import { StrengthPipe } from "./strength.pipe";
+
+describe('StrengthPipe', () => {
+    it('should display weak if strength is 5', () => {
+        let pipe = new StrengthPipe();
+
+        const val = pipe.transform(5);
+
+        expect(val).toEqual('5 (weak)');
+    });
+
+    it('should display strong if strength is 10', () => {
+        let pipe = new StrengthPipe();
+
+        const val = pipe.transform(10);
+
+        expect(val).toEqual('10 (strong)');
+    });
+
+    it('should display unbelievable if strength is 20', () => {
+        let pipe = new StrengthPipe();
+
+        const val = pipe.transform(20);
+
+        expect(val).toEqual('20 (unbelievable)');
+    });
+});
